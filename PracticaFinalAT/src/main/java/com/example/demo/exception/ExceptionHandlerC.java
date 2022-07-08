29e60bf5597582cb6abd7a75d4e84757f94b1136
Jorge Vélez <jvelezbueso@gmail.com>
2022-07-08 19:30:45 +0200

    Quitar dependencias

--- PracticaFinalAT/src/main/java/com/example/demo/exception/ExceptionHandlerC.java
+++ PracticaFinalAT/src/main/java/com/example/demo/exception/ExceptionHandlerC.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.att.ajsc.common.error.ErrorDetails;
 
 public class ExceptionHandlerC extends ResponseEntityExceptionHandler {
 
